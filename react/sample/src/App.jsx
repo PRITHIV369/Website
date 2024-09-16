7fc3e88331bfad02870e7d31ab4ed179a03e74ae
prithiv369 <prithivraj369@gmail.com>
2024-09-16 16:22:54 +0530

    r3-commit

--- react/sample/src/App.jsx
+++ react/sample/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import Navbar from './components/Navbar'
+const App = () => {
+  const appname="WELCOME"
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full">
+      <Navbar appname={appname} />
+    </div>
+    <div className="h-screen w-screen flex justify-center bg-slate-900 items-center">
+      <div className="h-[60%] w-[30%] flex justify-center items-center bg-slate-200 rounded-md shadow-4xl">
+      <form className="h-[90%] w-[80%] flex flex-col justify-center items-center gap-3">
+        <b>LOGIN</b>
+        <input type="text" placeholder="name" className="p-3 w-full bg-slate-300 outline-none focus:border-b-2 focus:border-gray-800" />
+        <input type="email" placeholder="email" className="p-3 w-full  bg-slate-300 outline-none focus:border-b-2 focus:border-gray-800" />
+        <input type="password" placeholder="password" className="p-3 w-full  bg-slate-300 outline-none focus:border-b-2 focus:border-gray-800" />
+        <button type="submit" className="w-full bg-gradient-to-r from-slate-900 to-purple-600 hover:bg-slate-600 text-slate-200 h-[15%] rounded-md font-bold">SUBMIT</button>
+        <a href='' className=''><small>Forget Password?</small></a>
+      </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
-
 export default App
---
+++ react/sample/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = ({appname}) => {
+  return (
+    <div className="bg-purple-900 text-white ">
+        {appname}
+    </div>
+  )
+}
+
+export default Navbar
